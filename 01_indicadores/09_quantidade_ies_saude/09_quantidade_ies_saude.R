6631a026e41c84832afb54434d0b543205533144
admhenri <henriqueribeiro@discente.ufg.br>
2024-11-21 17:21:19 -0300

    .

--- 01_indicadores/09_quantidade_ies_saude/09_quantidade_ies_saude.R
+++ 01_indicadores/09_quantidade_ies_saude/09_quantidade_ies_saude.R
@@ -12,6 +12,7 @@ library(ggplot2)
 
 # Leitura dos dados -------------------------------------------------------
 
+
 dremio_host <- Sys.getenv("endereco")
 dremio_port <- Sys.getenv("port")
 dremio_uid <- Sys.getenv("uid")
@@ -30,6 +31,7 @@ channel <- odbcDriverConnect(sprintf("DRIVER=Dremio Connector;
                                      dremio_uid, 
                                      dremio_pwd))
 
+
 query <- 'SELECT * FROM "Open Analytics Layer"."Educação"."Quantidade de IES com cursos de saúde por município"'
 
 
@@ -38,33 +40,41 @@ IES <- sqlQuery(channel,
                 as.is = TRUE)
 
 
+
 # tratamento dos dados ----------------------------------------------------
+
 
 IES$qtd_ies_cursos <- as.integer(IES$qtd_ies_cursos)
 
+
 qtd_IES <- 
   IES |> 
-  filter(regiao == "Região Centro-Oeste",
-         ano %in% c(2018, 2019, 2021, 2022)) |> 
+  filter(cod_ibge %in% c("120040", "130260", "160030", "150140", "110020", "140010", "172100"),
+         ano %in% c(2021, 2022)) |> 
   group_by(ano, uf_sigla) |> 
   summarise(total = sum(qtd_ies_cursos), .groups = "drop")
 
 
+
 # Criação do gráfico ------------------------------------------------------------
+
 
 a <- ggplot(qtd_IES, aes(x = uf_sigla, y = total, fill = factor(ano))) + 
   geom_col(position = "dodge") +
   geom_text(aes(label = total), position = position_dodge(width = 0.9), vjust = -0.5, size = 5) +
-  ggtitle("Comparação",
+  ggtitle("Comparação da Quantidade de IES nas Capitais do Norte do Brasil entre 2021 e 2022",
           "Fonte: Censo da Educação Superior") +
-  labs(x = "Estado", 
+  labs(x = "Capital", 
        y = "Total", 
        fill = "Ano") +
   theme_minimal() +
-  scale_x_discrete(labels = c("GO" = "Goiás", 
-                              "DF" = "Distrito Federal", 
-                              "MS" = "Mato Grosso do Sul", 
-                              "MT" = "Mato Grosso")) +
+  scale_x_discrete(labels = c("AC"="Rio Branco", 
+                              "AM"="Manaus", 
+                              "AP"="Macapá", 
+                              "PA"="Belém", 
+                              "RO"="Porto Velho", 
+                              "RR"="Boa Vista", 
+                              "TO"="Palmas")) +
   theme(
     plot.title = element_text(size = 20, face = "bold"), 
     plot.subtitle = element_text(size = 16),
@@ -78,7 +88,11 @@ a <- ggplot(qtd_IES, aes(x = uf_sigla, y = total, fill = factor(ano))) +
     legend.text = element_text(size = 14)
   )
 
+
 a
 
-ggsave(filename = "razao_equipamentos.jpeg", plot = a,
+
+ggsave(filename = "quantidade_ies_saude.jpeg", plot = a,
        dpi = 400, width = 16, height = 8)
+
+
