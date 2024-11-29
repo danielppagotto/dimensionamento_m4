df549bfa78865e5c07f83cfc57720418e16811e8
Henrique <107931165+admhenri@users.noreply.github.com>
2024-11-29 16:15:40 -0300

    Create 19_razao_equip_odonto.sql

---
+++ 01_indicadores/19_razao_equip_odonto/19_razao_equip_odonto.sql
@@ -0,0 +1,57 @@
+WITH EQUIPAMENTOS AS (
+    SELECT
+        CAST(SUBSTRING(COMPETEN, 1, 4) AS INTEGER) AS ano,
+        CASE
+            WHEN LENGTH(CODUFMUN) = 7 THEN SUBSTR(CODUFMUN, 1, 6)
+            WHEN CODUFMUN LIKE '53%' THEN '530010'
+            ELSE CODUFMUN
+        END AS cod_ibge,
+        CASE
+            WHEN CODEQUIP = '80' THEN 'Equipamento odontológico completo'
+            WHEN CODEQUIP = '81' THEN 'Compressor odontológico'
+            WHEN CODEQUIP = '82' THEN 'Fotopolimerizador'
+            WHEN CODEQUIP = '83' THEN 'Caneta de alta rotação'
+            WHEN CODEQUIP = '84' THEN 'Caneta de baixa rotação'
+            WHEN CODEQUIP = '85' THEN 'Amalgamador'
+            WHEN CODEQUIP = '86' THEN 'Aparelho de profilaxia c/ jato de bicarbonato'
+        END AS equipamento,
+        SUM(CASE WHEN IND_SUS = '1' THEN QT_EXIST ELSE 0 END) AS quantidade_equip_sus,
+        SUM(CASE WHEN IND_NSUS = '1' THEN QT_EXIST ELSE 0 END) AS quantidade_equip_nao_sus
+    FROM 
+        Dados.cnes.EQ eq
+    WHERE 
+        SUBSTRING(COMPETEN, 5, 2) = '01' AND
+        ano >= 2010 AND
+        TIPEQUIP = '7' AND
+        CODEQUIP IN ('80', '81', '82', 
+                    '83', '84', '85', 
+                    '86')
+    GROUP BY
+        ano,
+        CODEQUIP,
+        cod_ibge
+)
+
+SELECT 
+    e.ano,
+    p.regiao,
+    p.uf_sigla,
+    p.cod_macrorregiao,
+    p.macrorregiao,
+    p.cod_regsaud,
+    p.regiao_saude,
+    e.cod_ibge,
+    p.municipio,
+    p.latitude,
+    p.longitude,
+    e.equipamento,
+    e.quantidade_equip_sus,
+    e.quantidade_equip_nao_sus,
+    p.populacao,
+    CAST(quantidade_equip_sus AS FLOAT) / CAST(p.populacao AS FLOAT) * 10000 AS equip_pop_sus,        
+    CAST(quantidade_equip_nao_sus AS FLOAT) / CAST(p.populacao AS FLOAT) * 10000 AS equip_pop_nao_sus
+FROM 
+    EQUIPAMENTOS e
+LEFT JOIN 
+    "Open Analytics Layer".Territorial."População SVS por município e ano" p
+    ON e.cod_ibge = p.cod_ibge AND e.ano = p.ano
