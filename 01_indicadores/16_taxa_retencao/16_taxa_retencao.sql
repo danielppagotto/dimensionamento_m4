ee6889533114370a26e3b50f687b28f56f90885b
Henrique <107931165+admhenri@users.noreply.github.com>
2025-02-17 10:19:21 -0300

    Update 16_taxa_retencao.sql

--- 01_indicadores/16_taxa_retencao/16_taxa_retencao.sql
+++ 01_indicadores/16_taxa_retencao/16_taxa_retencao.sql
@@ -1,56 +1,50 @@
 WITH retencao AS (
 ----Cirurgiões-dentistas
     SELECT
-        SUBSTRING(ano, 1, 4) AS ano,
         regiao_saude AS cod_regsaud,
         'Cirurgiões-dentistas' as categoria,
-        retention AS taxa
+        retencao_geral AS taxa
     FROM
-    Dados.retencao."Cirurgiões-dentistas_retencao_ano.parquet"    
+        Dados.retencao."Cirurgiões-dentistas_retencao_geral.parquet"  
     UNION ALL
 
 ---- Médicos
     SELECT
-        SUBSTRING (ano, 1, 4) AS ano,
         regiao_saude AS cod_regsaud,
         'Médicos' AS categoria,
-        retention AS taxa
+        retencao_geral AS taxa
     FROM
-        Dados.retencao."Médico_retencao_ano.parquet"
+        Dados.retencao."Médico_retencao_geral.parquet"
     UNION ALL
 
 ---- Enfermeiros
     SELECT
-        SUBSTRING (ano, 1, 4) AS ano,
         regiao_saude AS cod_regsaud,
         'Enfermeiros' AS categoria,
-        retention AS taxa
+        retencao_geral AS taxa
     FROM
-        Dados.retencao."Enfermeiro_retencao_ano.parquet"
+        Dados.retencao."Enfermeiro_retencao_geral.parquet"
     UNION ALL
 
 ---- Técnicos e Auxiliares de Enfermagem
     SELECT
-    SUBSTRING(ano, 1, 4) AS ano,
-    regiao_saude AS cod_regsaud,
-    'Técnicos e Aux. de Enfermagem' AS categoria,
-    retention AS taxa
+        regiao_saude AS cod_regsaud,
+        'Técnicos e Aux. de Enfermagem' AS categoria,
+        retencao_geral AS taxa
     FROM
-        Dados.retencao."Técnicos e auxiliares de enfermagem_retencao_ano.parquet"
+        Dados.retencao."Técnicos e auxiliares de enfermagem_retencao_geral.parquet"
     UNION ALL
 
 ---- Técnicos e Auxiliares de Saúde Bucal
     SELECT
-    SUBSTRING(ano, 1, 4) AS ano,
-    regiao_saude AS cod_regsaud,
-    'Técnicos e Aux. de Saúde Bucal' AS categoria,
-    retention AS taxa
+        regiao_saude AS cod_regsaud,
+        'Técnicos e Aux. de Saúde Bucal' AS categoria,
+        retencao_geral AS taxa
     FROM
-        Dados.retencao."Técnico ou Auxiliar de Saúde Bucal_retencao_ano.parquet"
+        Dados.retencao."Técnico ou Auxiliar de Saúde Bucal_retencao_geral.parquet"
 )
 
 SELECT DISTINCT
-    a.ano,
     b.regiao,
     b.uf_sigla,
     a.cod_regsaud,
